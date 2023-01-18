175cfc0a73348932f7e0b49d0b42f6aacdb0f869
Zept0n <104791727+Zept0n@users.noreply.github.com>
2023-01-18 18:52:09 +0200

    feat:task 91 creating el for every tick

---
+++ src/js/Application.js
@@ -0,0 +1,35 @@
+import EventEmitter from "eventemitter3";
+import Beat from "./Beat";
+
+export default class Application extends EventEmitter {
+  static get events() {
+    return {
+      READY: "ready",
+    };
+  }
+
+  constructor() {
+    super();
+
+    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
+    let count = 0;
+
+    this._beat = new Beat();
+    this._beat.on(Beat.events.BIT, () => {
+      this._create(lyrics[count])
+      count++;
+      if (count>=lyrics.length) {
+        count=0;
+      }
+    });
+
+    this.emit(Application.events.READY);
+  }
+
+  _create(lyric) {
+    let message = document.createElement("div");
+    message.classList.add("message");
+    message.innerText = lyric;
+    document.querySelector(".main").appendChild(message);
+  };
+}
