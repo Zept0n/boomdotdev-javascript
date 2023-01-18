285f1f723bbd7a37c25422a2762c57fc30f20a87
Zept0n <104791727+Zept0n@users.noreply.github.com>
2023-01-19 00:25:42 +0200

    feat:task 86 Uninstalling modules, filter

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,16 @@
 import "../scss/app.scss";
-import { pluck } from 'ramda';
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const arrayToPluck = [
-    { name: "John", class: "is-primary" },
-    { age: 23, class: "is-warning" },
-    { job: "programmer", class: "is-danger" },
-  ];
-
-  let classes = pluck('class',arrayToPluck);
-  console.log(classes);
-
-  const articles = document.querySelectorAll("article");
-  for (let i=0;i<articles.length;i++) {
-    articles[i].classList.add(classes[i]);
-  };
-  
+  const ul = document.querySelector("ul");
+  const numbersArray = [1, 3, 4, 5, 6, 7];
+  const isEven = (n) => n % 2 === 0;
+  const filteredArray = numbersArray.filter(isEven);
+  const fragment=document.createDocumentFragment();
+  for (let index=0;index<filteredArray.length;index++) {
+    const li=document.createElement('li');
+    li.textContent=filteredArray[index];
+    fragment.appendChild(li);
+  }
+  ul.appendChild(fragment);
 });
